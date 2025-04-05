ef8fde4a6baf3571985db16cf3c50d917486b047
Aman Kumar <kraman102003@gmail.com>
2025-04-05 16:01:31 +0530

    pic correction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import avatar from "../assets/avatar.webp"
+import aman_professional_pic1 from "../assets/aman_professional_pic1-removebg-preview.png";
 import { service } from './data'
 
 
@@ -12,7 +12,7 @@ const About = () => {
                 <h1 className='text-white font-bold text-3xl mb-2'>About Me</h1>
                 <hr className='border-3 text-yellow-500 w-12 rounded-4xl' />
                 <div className='mt-5 md:hidden'>
-                    <img src={avatar} alt="" className='bg-gray-700 rounded-2xl' />
+                    <img src={aman_professional_pic1} alt="" className='bg-gray-700 rounded-2xl' />
                 </div>
                 <p className='mt-4 text-gray-300 text-xl font-semibold'>Hi! I'm Aman Kumar, a passionate Front-End Developer with expertise in React, JavaScript, and CSS. With a strong foundation in modern web development and an eye for detail, I create elegant, user-friendly web interfaces tailored to meet client needs.</p>
                 <p className='mt-2 text-gray-300 text-xl font-semibold'>With a strong foundation in IT, I’ve built my journey around a passion for web development, problem-solving, and creating user-centric digital experiences. I’m always eager to learn, explore new technologies, and take on challenges that push my limits. I’ve worked on various projects that enhanced my skills in frontend development, collaboration, and delivering real-world solutions. Let’s connect and bring innovative ideas to life together!</p>
